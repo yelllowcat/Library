c3632ec61b4860917efd9506df382b6211dadd40
David <davigonzalezdev@gmail.com>
2025-06-17 22:29:36 -0700

    dynamics templates with flexbox to see the books in the ui maked with javascript

--- main.js
+++ main.js
@@ -41,6 +41,52 @@ Book.prototype.madeBy = function () {
 };
 
 console.log();
-addBookToLibrary("Habits", "James Clear", 10);
+addBookToLibrary("Atomic Habits", "James Clear", 10);
+addBookToLibrary("The Power of Now", "Eckhart Tolle", 15);
+addBookToLibrary("Deep Work", "Cal Newport", 12);
+addBookToLibrary("The Subtle Art of Not Giving a F*ck", "Mark Manson", 14);
+addBookToLibrary("Thinking, Fast and Slow", "Daniel Kahneman", 18);
+addBookToLibrary("Can't Hurt Me", "David Goggins", 16);
+addBookToLibrary("Start With Why", "Simon Sinek", 11);
+addBookToLibrary("12 Rules for Life", "Jordan B. Peterson", 20);
+addBookToLibrary("Make Your Bed", "William H. McRaven", 9);
+addBookToLibrary("Mindset", "Carol S. Dweck", 13);
+addBookToLibrary("Grit", "Angela Duckworth", 17);
+addBookToLibrary("Digital Minimalism", "Cal Newport", 10);
+addBookToLibrary("Ego Is the Enemy", "Ryan Holiday", 11);
+addBookToLibrary(
+  "The 7 Habits of Highly Effective People",
+  "Stephen R. Covey",
+  19
+);
+addBookToLibrary("The Four Agreements", "Don Miguel Ruiz", 8);
+addBookToLibrary("The Miracle Morning", "Hal Elrod", 13);
+addBookToLibrary(
+  "How to Win Friends and Influence People",
+  "Dale Carnegie",
+  16
+);
+
 console.log(myLibrary);
 listBooks();
+function changeColor() {
+  let rand1 = getRandomInt(256);
+  let rand2 = getRandomInt(256);
+  let rand3 = getRandomInt(256);
+
+  return "rgb(" + rand1 + "," + rand2 + "," + rand3 + ")";
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+const container = document.querySelector(".container");
+myLibrary.forEach((element) => {
+  let divs = document.createElement("div");
+  divs.style.width = "22vh";
+  divs.style.height = "22vh";
+  divs.style.textAlign = "center";
+  let color = changeColor();
+  divs.style.background = color;
+  divs.innerHTML = element.title;
+  container.appendChild(divs);
+});
